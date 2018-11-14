6c89a20a15b5487f5e625b36ef7238e3d96acced
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-11-14 14:47:41 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef127bf0905bf8635d67afb9c3ae9fe8.js"
+  "/precache-manifest.0ed2da9ca3e616fb8ec27ad6ca0d33ef.js"
 );
 
 workbox.clientsClaim();
